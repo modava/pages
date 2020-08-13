512db9ef76683e4f9276fa521c2aa11cac90cc9e
Mong Dao <mongdaovan86@yahoo.com>
2020-08-13 17:30:50 +0700

    update 13082020

--- src/models/query/DocumentQuery.php
+++ src/models/query/DocumentQuery.php
@@ -25,4 +25,9 @@ class DocumentQuery extends \yii\db\ActiveQuery
     {
         return $this->orderBy([Document::tableName() . '.id' => SORT_DESC]);
     }
+
+    public function findByLanguage()
+    {
+        return $this->andWhere([Document::tableName() . '.language' => \Yii::$app->language]);
+    }
 }
